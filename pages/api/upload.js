4e174fc7cbc45aaa844e3f458eb25d54bb35cf24
Matthew-uk <esaduviedede@gmail.com>
2023-11-25 12:38:00 +0100

    Making the code better.

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,14 +1,14 @@
 import multiparty from "multiparty";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import fs from "fs";
 import mime from "mime-types";
 import { mongooseConnect } from "@/lib/mongoose";
-// import {isAdminRequest} from "@/pages/api/auth/[...nextauth]";
+// import { isAdminRequest } from "@/pages/api/auth/[...nextauth]";
+
 const bucketName = "dawid-next-ecommerce";
 
 export default async function handle(req, res) {
   await mongooseConnect();
-  // await isAdminRequest(req,res);
+  // await isAdminRequest(req, res);
 
   const form = new multiparty.Form();
   const { fields, files } = await new Promise((resolve, reject) => {
@@ -17,30 +17,28 @@ export default async function handle(req, res) {
       resolve({ fields, files });
     });
   });
-  console.log("length:", files.file.length);
-  const client = new S3Client({
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-  });
+
   const links = [];
+
   for (const file of files.file) {
-    const ext = file.originalFilename.split(".").pop();
-    const newFilename = Date.now() + "." + ext;
-    await client.send(
-      new PutObjectCommand({
-        Bucket: bucketName,
-        Key: newFilename,
-        Body: fs.readFileSync(file.path),
-        ACL: "public-read",
-        ContentType: mime.lookup(file.path),
-      })
-    );
-    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
-    links.push(link);
+    try {
+      const ext = file.originalFilename.split(".").pop();
+      const newFilename = Date.now() + "." + ext;
+      const newPath = `${newFilename}`; // Adjust the path accordingly
+
+      // Read the file and save it locally
+      const fileBuffer = fs.readFileSync(file.path);
+      fs.writeFileSync(newPath, fileBuffer);
+
+      const link = `${newPath}`;
+      console.log(newPath);
+      links.push(link);
+    } catch (error) {
+      console.error("Error saving file locally:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
   }
+
   return res.json({ links });
 }
 
